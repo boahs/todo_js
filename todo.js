f662772b7dd5ddfe9609f2ddf29dff6311271145
boahs <39522778+boahs@users.noreply.github.com>
2020-01-18 21:27:09 -0500

    add function

--- todo.js
+++ todo.js
@@ -7,3 +7,8 @@ var todos = ['item 1', 'item 2', 'item3', 'item4', 'item 5'];
     }
 
     displayTodos()
+
+    function addTodo(todos) {
+        todos.push(todos);
+        displayTodos();
+    }
