1957164cf077a8c79ef5ff91887a63d703e92965
boahs <39522778+boahs@users.noreply.github.com>
2020-01-19 15:27:12 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,18 +1,27 @@
 var todos = ['item 1', 'item 2', 'item3', 'item4', 'item 5'];
+var myName = 'Boahs';
 
+
+
+    function sayName() {
+        console.log(myName);
+    }
 
     function displayTodos() {
         console.log('My Todos:', todos);
-
     }
 
-    function addTodos(todos) {
-        todos.push(todos);
+    function addTodos(todo) {
+        todos.push(todo);
         displayTodos();
     }
 
     function changeTodos(position, newValue) {
         todos[position] = newValue;
+    }
+
+    function deleteTodos(position) {
+        todos.splice(position, 1);
     }
 
 
