2fc11ce2c8b12deee9c988c9b072f0e18f865fac
boahs <39522778+boahs@users.noreply.github.com>
2020-01-19 23:17:50 -0500

    replaced addTodo function with method

--- todo.js
+++ todo.js
@@ -2,7 +2,13 @@ var todoList = {
     todos: ['item 1', 'item 2', 'item3', 'item4', 'item 5'],
     displaytodos: function() {
         console.log('my todos', this.todos)
+    },
+
+    addTodo: function(todo) {
+        this.todos.push(todo)
+        this.displaytodos();
     }
+    
 };
 
 var myName = 'Boahs';
@@ -11,10 +17,6 @@ var myName = 'Boahs';
 
     function sayName() {
         console.log(myName);
-    }
-
-    function addTodos(todos) {
-        todos.push(todos);
     }
 
     function changeTodos(position, newValue) {
