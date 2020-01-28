aba575b01e01bfb9ad8c4076f2dc34567c27333b
boahs <39522778+boahs@users.noreply.github.com>
2020-01-28 02:44:00 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -49,8 +49,16 @@ var todoList = {
     toggleAll: function() {
         var totalTodos = this.todos.length;
         var completedTodos = 0;
-        if (completedTodos === totalTodos) {
 
+        for (var i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true){
+                completedTodos++;
+            }
+        }
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < totalTodos; i++){
+                this.todos[i].completed = false;
+            }
         }
     }
 };
